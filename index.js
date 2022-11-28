843c6bfadfdff1a2c357050d6f6448988e1b72ee
Jesse <jessemutua76@gmail.com>
2022-11-28 11:28:02 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName;
+customerName = "bob";
+customerName;
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    var setBestCustomer = "not bob"
+    return setBestCustomer;
+}
+setBestCustomer();
